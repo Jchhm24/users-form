17d93147182c829bf0fa629bd27159e8563657d6
Jchhm24 <jcchm2004@gmail.com>
2025-02-15 13:20:45 -0600

    Agregar interfaces para Archivo, Localidad y Municipio en types.ts

---
+++ src/interfaces/types.ts
@@ -0,0 +1,19 @@
+export interface Archivo {
+    name: string;
+    download_url: string;
+}
+
+export interface Localidad {
+    clave: string;
+    nombre: string;
+}
+
+export interface Municipio {
+    clave: string;
+    nombre: string;
+}
+
+// lo que imprime el console.log
+
+//clave: "310010001"
+//nombre: "Abalá"
